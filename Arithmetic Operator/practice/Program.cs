5e0bd9b4de825ef11138889ba126f7d49c22d89f
Gurukiran Patrimath <gurukiranp@infinite.com>
2024-06-19 13:26:24 +0530

    commit

---
+++ Arithmetic Operator/practice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Arithmatic operator
+            // Console.WriteLine("Enter the first number");
+            //int a = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter the second number");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("The value of a+b is: " + (a + b));
+            //Console.WriteLine("The value of a - b is: " + (a -b));
+            //Console.WriteLine("The value of a+b is:  " +(a -b));
+            //Console.WriteLine("The value of a+b is:  " +(a /b));
+            // Console.ReadLine();
+
+            //Assignment operator
+            Console.WriteLine("Enter the Name");
+            string name = Console.ReadLine();
+
+             Console.WriteLine("Enter the  number");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the  number2");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The value of a+b is: " + (a + b));
+            Console.WriteLine("The value of a - b is: " + (a -b));
+            Console.WriteLine("The value of a+b is:  " +(a -b));
+            Console.WriteLine("The value of a+b is:  " +(a /b));
+             Console.ReadLine();
+        }
+    }
+}
